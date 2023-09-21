ebbd55271587d24e4940d03b7f709a335dd7e90a
ayuuumi119 <144981053+ayuuumi119@users.noreply.github.com>
2023-09-21 04:38:17 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
-st.title("おみくじアプリ")
-if st.button("おみくじを引く"):
+st.title("テンションアゲアゲアプリ")
+if st.button("引く"):
     results = ["大吉","大大吉","お疲れ様！","きっといいことがあるよ","明日も頑張ってね","よく頑張ったね"]
     result =random.choice(results)
     st.write(f"結果:{result}")
